48045b19095260bb70581efcdc629e2e9af9f2e1
Samuel dos Santos Conceição <160559799+samueldossantos1@users.noreply.github.com>
2024-05-20 22:57:52 -0300

    aula 11 de JavaScript

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 70
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+if (vel > 60) { //condição simples!
+    console.log('você ultrapassou a velocidade permitida. MULTADO!')
+}
+console.log('Dirja sempre usando o cinto de segurança!')
