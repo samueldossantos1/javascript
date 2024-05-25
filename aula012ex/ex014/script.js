c2e918c759249d7a474480aac9cd149dcf6af51d
Samuel dos Santos Conceição <160559799+samueldossantos1@users.noreply.github.com>
2024-05-25 14:38:00 -0300

    Atualizei modelo.html

--- aula012ex/ex014/script.js
+++ aula012ex/ex014/script.js
@@ -1,1 +1,22 @@
-alert ('Ola!')
+function carregar() {
+var msg  = window.document.getElementById('msg');
+var img = window.document.getElementById('imagem');
+var data = new Date();
+var hora = data.getHours();
+var minu = data.getMinutes();
+msg.innerHTML = `Agora são <strong>${hora} horas</strong> e <strong>${minu} minutos.</strong>`;
+if (hora < 12) {
+    texto.innerHTML = 'BOM DIA!';
+    img.src = 'foto-manha.png';
+    document.body.style.background = '#8F7621';
+} else if (hora < 18) {
+    texto.innerHTML = 'BOA TARDE!';
+    img.src = 'foto-tarde.png';
+    document.body.style.background = '#FC8801';
+} else {
+    texto.innerHTML = 'BOA NOITE!';
+    img.src = 'foto-noite.png';
+    document.body.style.background = '#01253B';
+}
+
+}
